a477e8b4d4536d76ecb35d42c571aceb7de5314b
Daniel <46136@gymnasium.ax>
2021-11-25 10:50:54 +0200

    Kapitel 2 fil

---
+++ Kapitel2/Uppgifter2Fram.php
@@ -0,0 +1,9 @@
+<?php
+
+$days = array ("Måndag","Tisdag","Onsadg","Torsdag","Fredag","Lördag","Söndag");
+var_dump($days);
+print_r($days);
+
+$manader = array ("jan","feb","mar","apr","maj","juni","juli","aug","sep","okt","nov","dec");
+var_dump($manader);
+print_r($manader);
