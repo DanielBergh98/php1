0cc520d752330d2e1073754da915bb6033b2778b
Daniel <46136@gymnasium.ax>
2021-11-25 10:47:22 +0200

    Kapitel 1 fil

---
+++ Kapitel1/5.1.svar.php
@@ -0,0 +1,14 @@
+<?php
+$namn=$_GET['namn'];
+$adress=$_GET['adress'];
+$postnr=$_GET['postnr'];
+$ort=$_GET['ort'];
+
+
+echo "Du skrev in förljande:<br>";
+echo  "Namn: <b>$namn</b><br>";
+echo  "Adress: <b>$adress</b><br>";
+echo  "Postnr: <b>$postnr</b><br>";
+echo  "Ort: <b>$ort</b><br>";
+
+    
