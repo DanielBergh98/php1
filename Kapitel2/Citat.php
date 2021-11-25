464f68e837bcd2e1a0fc133564077b438eac06df
Daniel <46136@gymnasium.ax>
2021-11-25 10:50:21 +0200

    Kapitel 2 fil

---
+++ Kapitel2/Citat.php
@@ -0,0 +1,6 @@
+<?php
+
+$citat = array ("\"The love of money is the root of all evil. - bibel\"","\"The only thing we have to fear is fear itself. - Franklin D. Roosevelt \"");
+$rand_keys = array_rand($citat, 2);
+echo $citat;
+?>
