6069642a8b3df17c7a32c6bafeca83bfae17e442
Daniel <46136@gymnasium.ax>
2021-11-25 10:47:44 +0200

    Kapitel 1 fil

---
+++ Kapitel1/5.7.svar.php
@@ -0,0 +1,12 @@
+<?php
+
+$favoritFarg=$_POST['favoritFarg'];
+
+?>
+<!DOCTYPE html>
+<html>
+    <body style="background-color: <?= $favoritFarg; ?>">
+    <p style="background-color: white"> ohhh vad vackert! </p>
+        
+    </body>
+</html>
