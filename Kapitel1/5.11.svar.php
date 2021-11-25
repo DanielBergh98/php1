86cb68ac07b27e6d7e30cb7eae14fb537567fb50
Daniel <46136@gymnasium.ax>
2021-11-25 10:47:58 +0200

    Kapitel 1 fil

---
+++ Kapitel1/5.11.svar.php
@@ -0,0 +1,20 @@
+<?php
+
+$namn=$_POST['namn'];
+$fodelsedag=$_POST['fodelsedag'];
+$user=$_POST['user'];
+$losenord=$_POST['losenord'];
+$Telefon=$_POST['Telefon'];
+
+
+echo "Du skrev in förljande:<br>";
+echo  "Namn: <b>$namn</b><br>";
+echo  "Födelsedag: <b>$fodelsedag</b><br>";
+echo  "Lösenord: <b>$losenord</b><br>";
+echo  "Telefon: <b>$Telefon</b><br>";
+
+
+<div>
+<p>Föredrar du sommar eller vinter?</p>
+<label for="svar3"</label>
+</div>
