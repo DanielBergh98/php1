000770a63f311dc0497a2f908782e0bdc4487d48
Daniel <46136@gymnasium.ax>
2021-11-25 10:57:49 +0200

    Kapitel 4 fil

---
+++ Strängoperatorer.php
@@ -0,0 +1,13 @@
+<?php
+
+$a='Kalle ' . 'Nilsson'; // Kalle Nilsson
+echo $a . "<br>";
+
+$b="Pelle" . 'Persson'; // PellePersson
+echo $b . '<br>';
+
+$c='Kjell';
+$c.=" Hansen";
+
+echo 'jag heter $c <br>'; // Jag heter $c
+echo "Jag Heter $c <br>"; // Jag heter Kalle Nilsson
